76c7482e7a6126832fd6c39bbf43d3242ad33abb
NotWeisai <147244945+NotWeisai@users.noreply.github.com>
2023-10-27 15:58:59 +0300

    Update Homework2.3.py

--- HomeworksCources/Homework2.3.py
+++ HomeworksCources/Homework2.3.py
@@ -2,7 +2,7 @@ print('Введите координату клетки по оси x:')
 x = int(input())
 print('Введите координату клетки по оси y:')
 y = int(input())
-if ((x % 2 == 0) or (y % 2 == 0)) and (x != y):
-    print('YES')
-else:
+if (x+y) % 2 == 0:
     print('NO')
+else:
+    print('YES')
